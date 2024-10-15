6fa946cf810a131546d68413e014220d376e8ee3
anhtienpro25 <trunglh113@gmail.com>
2024-10-15 22:23:11 +0700

    add

--- src/model/fusion.py
+++ src/model/fusion.py
@@ -13,29 +13,47 @@ class ConvForward(nn.Module):
         self.conv1 = nn.Conv2d(d_model, d_model * 2, kernel_size=1)
         self.bn = nn.BatchNorm2d(d_model * 2)
         self.drop = nn.Dropout(dropout_rate)
-        self.relu = nn.ReLU()
+        self.ge = nn.GELU()
         self.conv2 = nn.Conv2d(d_model * 2, d_model, kernel_size=1)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn(x)
-        x = self.relu(x)
+        x = self.ge(x)
         x = self.drop(x)
         x = self.conv2(x)
         return x
 
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, dropout_rate: float):
+        super(FeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_model * 2)
+        self.bn = nn.LayerNorm(d_model * 2)
+        self.drop = nn.Dropout(dropout_rate)
+        self.ge = nn.GELU()
+        self.fc2 = nn.Linear(d_model * 2, d_model)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.bn(x)
+        x = self.ge(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        return x
+
 
 class Classifer(nn.Module):
-    def __init__(self, input_size, num_classes):
+    def __init__(self, input_size, num_classes, dropout_rate=0.2):
         super(Classifer, self).__init__()
-        # self.pooling = nn.AdaptiveMaxPool2d(1)
         self.flatten = nn.Flatten()
+        self.dff = FeedForward(input_size, dropout_rate)
         self.fc = nn.Linear(input_size, num_classes)
 
     def forward(self, x):
         kernel_size = x.size()[2:]
         x = F.avg_pool2d(x, kernel_size)
         x = self.flatten(x)
+        x = x + self.dff(x)
         x = self.fc(x)
         return x
 
