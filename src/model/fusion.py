558d0af1a8629c83d29c122e0aaa6058405a5f86
anhtienpro25 <trunglh113@gmail.com>
2024-10-16 14:02:40 +0700

    add

--- src/model/fusion.py
+++ src/model/fusion.py
@@ -59,7 +59,7 @@ class Classifer(pl.LightningModule):
 
     def forward(self, x):
         # kernel_size = x.size()[2:]
-        x = F.adaptive_avg_pool2d(x, 1)
+        x = F.adaptive_max_pool2d(x, 1)
         x = self.flatten(x)
 
         x = x + self.ffd(x)
