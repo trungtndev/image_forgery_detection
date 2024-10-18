d1f6c7e00a9e16e7876714e2aa0a10fd60ba1b67
anhtienpro25 <trunglh113@gmail.com>
2024-10-18 22:36:21 +0700

    add

--- src/lit_model.py
+++ src/lit_model.py
@@ -58,7 +58,7 @@ class LitModel(pl.LightningModule):
             optimizer,
             verbose=True,
             step_size=self.hparams.patience,
-            gamma=0.5)
+            gamma=0.25)
 
         scheduler = {
             "scheduler": step_scheduler,
