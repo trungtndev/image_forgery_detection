b7806f5fc82792da024dcf44f91ef7ccd38f0753
anhtienpro25 <trunglh113@gmail.com>
2024-10-21 07:53:52 +0700

    add

--- src/lit_model.py
+++ src/lit_model.py
@@ -47,7 +47,7 @@ class LitModel(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam([
             {'params': self.model.spatial.swinv1.parameters(), 'lr': self.hparams.learning_rate * 0.1},
-            {'params': self.model.spatial.out.parameters(), 'lr': self.hparams.learning_rate * 0.8},
+            {'params': self.model.spatial.out.parameters(), 'lr': self.hparams.learning_rate * 0.75},
 
             {'params': self.model.frequency.parameters(), 'lr': self.hparams.learning_rate},
             {'params': self.model.head.parameters(), 'lr': self.hparams.learning_rate},
