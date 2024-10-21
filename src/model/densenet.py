e801b1b565e16934c2ed5486a6766ba34d0d092a
anhtienpro25 <trunglh113@gmail.com>
2024-10-21 07:56:05 +0700

    add

--- src/model/densenet.py
+++ src/model/densenet.py
@@ -21,14 +21,11 @@ class _Bottleneck(nn.Module):
         self.use_dropout = use_dropout
         self.dropout = nn.Dropout(p=0.2)
 
-        # self.cbam = CBAM(channels=growth_rate, reduction_rate=2, kernel_size=7)
-
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)), inplace=True)
         if self.use_dropout:
             out = self.dropout(out)
         out = F.relu(self.bn2(self.conv2(out)), inplace=True)
-        # out = self.cbam(out)
         if self.use_dropout:
             out = self.dropout(out)
         out = torch.cat((x, out), 1)
